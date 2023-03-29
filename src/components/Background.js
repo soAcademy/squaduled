8e8bac8641bd7d45bcf8bcf422342095e0a405ab
Your name01~ <poovareskkc@gmail.com>
2023-03-29 10:10:58 +0700

    edit8

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,16 +1,19 @@
-// import React from 'react';
-// import Box from '@mui/material/Box';
+import React from 'react';
+import Box from '@mui/material/Box';
 
-// function MyComponent() {
-//   return (
-//     <Box
-//       sx={{
-//         backgroundImage: '("./components/im.png")',
-//         backgroundSize: 'cover',
-//         backgroundPosition: 'center',
-//       }}
-//     >
-//       {/* Your content goes here */}
-//     </Box>
-//   );
-// }
+const styles = {
+  backgroundImage: `url('https://cdn.pixabay.com/photo/2016/09/05/21/37/cat-1647775_960_720.jpg')`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  height: '100vh',
+};
+
+const MyComponent = () => {
+  return (
+    <Box sx={styles}>
+      {/* Your content goes here */}
+    </Box>
+  );
+}
+
+export default MyComponent;
