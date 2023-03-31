378b005849c08b7fa0e8d54aa181f3e95821be96
kunlanis <yoo.kunlanis@gmail.com>
2023-03-31 23:27:24 +0700

    refactor code ready to decorate

--- src/pages/ManagementList.js
+++ src/pages/ManagementList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+import { Button } from "@mui/material";
 import Typography from "@material-ui/core/Typography";
 import { useNavigate } from "react-router-dom";
 
@@ -31,49 +31,30 @@ const useStyles = makeStyles((theme) => ({
 const ManagementList = () => {
   const classes = useStyles();
   const navigate = useNavigate();
+  const lists = [
+    { manage: "จัดการห้องประชุม", path: "/manage-building" },
+    { manage: "จัดการสิ่งอำนวยความสะดวก", path: "/manage-facility" },
+    { manage: " จัดการวัน/เวลา ให้บริการ", path: "/manage-office-hour" },
+    { manage: "จัดการรายการจอง", path: "/room-booking-list" },
+  ];
 
   return (
     <div className={classes.root}>
       <Typography variant="h5" className={classes.title}>
         การจัดการ
       </Typography>
-      <Button
-        onClick={() => navigate("/manage-building")}
-        variant="contained"
-        size="large" // set size to large
-        className={classes.button}
-      >
-        จัดการอาคารและสถานที่
-      </Button>
-      <Button
-        onClick={() => navigate("/manage-facility")}
-        variant="contained"
-        size="large" // set size to large
-        className={classes.button}
-      >
-        จัดการสิ่งอำนวยความสะดวก
-      </Button>
-      <Button
-        onClick={() => navigate("/manage-office-hour")}
-        variant="contained"
-        size="large" // set size to large
-        className={classes.button}
-      >
-        จัดการวันที่/เวลา ให้บริการ
-      </Button>
-      <Button
-        onClick={() => navigate("/room-booking-list")}
-        variant="contained"
-        size="large" // set size to large
-        className={classes.button}
-      >
-        จัดการรายการจอง
-      </Button>
-      <Button
-        onClick={() => navigate("/room-searching")}
-       className="absolute bottom-8 left-8 px-6 py-2 rounded-lg bg-[#4A7654] hover:bg-[#6e9176] text-center text-gray-200 text-sm">
-        กลับ
-      </Button>
+      {lists.map((list) => {
+        return (
+          <Button
+            onClick={() => navigate(list.path)}
+            variant="contained"
+            size="large" // set size to large
+            className={classes.button}
+          >
+            {list.manage}
+          </Button>
+        );
+      })}
     </div>
   );
 };
